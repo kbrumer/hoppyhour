ff08167a5a4f86bcc6909cafa6e822fcefd85983
Sarah Joy Calpo <sajoy.pc@gmail.com>
2016-06-18 13:39:21 -0700

    add furtive css file

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import routes from './routes';
 import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import './styles/furtive.css'; 
 
 const store = configureStore();
 
